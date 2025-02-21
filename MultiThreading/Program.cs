d0aea222c982828a8d3e4a224cce792a1336ba02
navee <navee@NAVEEN>
2025-02-21 11:55:15 +0530

    Add project files.

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Thread: {i}");
+            Thread.Sleep(1000);  
+        }
+    }
+
+    static void Main()
+    {
+        Thread thread = new Thread(PrintNumbers);
+        thread.Start();  
+
+        Console.WriteLine("Main thread continues...");
+    }
+}
